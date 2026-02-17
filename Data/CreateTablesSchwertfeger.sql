3a2d694f5d550e2086dd2a949b08ed2a2222fb94
ncs00014 <ncs00014@mix.wvu.edu>
2026-02-17 12:28:51 -0500

    Updated ConferenceDivision table

--- Data/CreateTablesSchwertfeger.sql
+++ Data/CreateTablesSchwertfeger.sql
@@ -13,11 +13,36 @@ GO
 USE MIST353_NFL_Schwertfeger;
 GO
 
+if(OBJECT_ID('Team') is NOT NULL)
+    drop TABLE Team;
+if(OBJECT_ID('ConferenceDivision') is NOT NULL)
+    drop table ConferenceDivision;
+
+CREATE TABLE ConferenceDivision (
+    ConferenceDivisionID INT IDENTITY(1,1)
+        CONSTRAINT PK_ConferenceDivision PRIMARY KEY,
+    Conference NVARCHAR(50) NOT NULL
+        constraint CK_ConferenceNames CHECK (Division IN ('East', 'North', 'South', 'West')),
+    CONSTRAINT UK_ConferenceDivision UNIQUE (Conference, Division)        
+
+);
+
+/*
+alter table ConferenceDivision
+  NOCHECK constraint CK_ConferenceNames;
+
+alter table ConferenceDivision
+    CHECK CONSTRAINT CK_ConferenceNames;
+*/
+
+
 -- Create Team table
 CREATE TABLE Team (
     TeamID INT IDENTITY(1,1),
+        CONSTRAINT PK_Team PRIMARY KEY,
     TeamName NVARCHAR(50) NOT NULL,
-    City NVARCHAR(50) NOT NULL,
+    TeamCityState NVARCHAR(50) NOT NULL,
+    TeamColors NVARCHAR(50) NOT NULL,
     CONSTRAINT PK_Team PRIMARY KEY (TeamID)
 );
 GO
