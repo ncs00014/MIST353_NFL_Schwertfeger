9611752e12e5f476d46bb6e0938ba52dd3c53e58
ncs00014 <ncs00014@mix.wvu.edu>
2026-03-12 12:48:51 -0400

    Commited Today's Code

---
+++ API/nfl_playoffs_api.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from get_teams_by_conference_division import get_teams_by_conference_division
+app = FastAPI()
+
+@app.get("/teams")
+def read_teams(conference: str = None, division: str = None):
+    return get_teams_by_conference_division(conference=conference, division=division)
